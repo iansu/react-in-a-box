18db04fd3e0367175744a90428592181145597ff
Ian Sutherland <ian@iansutherland.ca>
2019-10-25 16:40:02 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# React In A Box
+
+It's React, in a box.
+
+Bootstraps an app with the `@next` version of Create React App.
+
+## Usage
+
+### `npx react-in-a-box <project-name>`
+
+Made with :heart: by [@iansu](https://twitter.com/iansu) and [@donavon](https://twitter.com/donavon)
---
+++ src/app.ts
@@ -0,0 +1,67 @@
+import chalk from 'chalk';
+import yargs, { Argv } from 'yargs';
+import { spawn } from 'child_process';
+
+interface Args {
+  name?: string;
+  'scripts-version'?: string;
+  template?: string;
+}
+
+const createApp = (args: Args): Promise<void> => {
+  return new Promise((resolve, reject): void => {
+    if (!args.name) {
+      console.error(chalk.red('You must specify a project name'));
+      reject();
+    }
+
+    if (args.name && args['scripts-version'] && args.template) {
+      const proc = spawn('create-react-app', [
+        `--scripts-version=${args['scripts-version']}`,
+        `--template=${args.template}`,
+        args.name
+      ]);
+
+      proc.stdout.on('data', (data): void => {
+        process.stdout.write(data);
+      });
+
+      proc.stderr.on('data', (data): void => {
+        process.stderr.write(data);
+      });
+
+      proc.on('close', (): void => {
+        resolve();
+      });
+    }
+  });
+};
+
+yargs
+  .scriptName('react-in-a-box')
+  .usage('$0 [name]')
+  .command({
+    command: '$0 [name]',
+    describe: 'create a new app',
+    builder: (yargs): Argv<{ name?: string }> =>
+      yargs.positional('name', {
+        required: true,
+        describe: 'app name',
+        type: 'string'
+      }),
+    handler: async (args: Args): Promise<void> => {
+      await createApp(args);
+    }
+  })
+  .demandCommand(1)
+  .option('scripts-version', {
+    default: '@next',
+    describe: 'react-scripts version',
+    type: 'string'
+  })
+  .option('template', {
+    default: 'cra-template@next',
+    describe: 'template package',
+    type: 'string'
+  })
+  .help().argv;
